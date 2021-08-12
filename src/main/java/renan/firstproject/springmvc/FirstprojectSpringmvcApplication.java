07ddf87bec6fcc0e550eacfaa4883d21a2b8bfa0
Renan <souza.rnn@gmail.com>
2021-08-12 00:45:32 -0300

    Project creation, adding dependencies to pom.xml, importing frontend files, controllers

---
+++ src/main/java/renan/firstproject/springmvc/FirstprojectSpringmvcApplication.java
@@ -0,0 +1,13 @@
+package renan.firstproject.springmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstprojectSpringmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstprojectSpringmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/renan/firstproject/springmvc/FirstprojectSpringmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package renan.firstproject.springmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstprojectSpringmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
