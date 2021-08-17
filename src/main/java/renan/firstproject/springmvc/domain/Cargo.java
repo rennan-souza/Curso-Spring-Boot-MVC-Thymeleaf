af4c5dbbf0007f9e99d30a120b94e4ec3bf8493e
Renan <souza.rnn@gmail.com>
2021-08-17 00:03:25 -0300

    been validation, department and position validations

--- src/main/java/renan/firstproject/springmvc/domain/Cargo.java
+++ src/main/java/renan/firstproject/springmvc/domain/Cargo.java
@@ -8,6 +8,10 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
 
 @Entity
 @Table(name = "CARGOS")
@@ -15,9 +19,12 @@ public class Cargo extends AbstractEntity<Long> {
 
 	private static final long serialVersionUID = 1L;
 	
+	@NotBlank(message = "O nome do cargo é obrigatório.")
+	@Size(max = 60, message = "O nome do cargo deve conter no máximo 60 caracteres.")
 	@Column(name = "nome", nullable = false, unique = true, length = 60)
 	private String nome;	
 	
+	@NotNull(message = "Selecione o departamento relativo ao cargo.")
 	@ManyToOne
 	@JoinColumn(name = "id_departamento_fk")
 	private Departamento departamento;
--- src/main/java/renan/firstproject/springmvc/web/controller/DepartamentoController.java
+++ src/main/java/renan/firstproject/springmvc/web/controller/DepartamentoController.java
@@ -1,8 +1,11 @@
 package renan.firstproject.springmvc.web.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -25,7 +28,12 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/salvar")
-	public String salvar(Departamento departamento, RedirectAttributes attr) {
+	public String salvar(@Valid Departamento departamento, BindingResult result, RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "/departamento/cadastro";
+		}
+		
 		service.salvar(departamento);
 		attr.addFlashAttribute("success", "Departamento inserido com sucesso.");
 		return "redirect:/departamentos/cadastrar";
@@ -44,7 +52,12 @@ public class DepartamentoController {
 	}
 	
 	@PostMapping("/editar")
-	public String editar(Departamento departamento, RedirectAttributes attr) {
+	public String editar(@Valid Departamento departamento, BindingResult result, RedirectAttributes attr) {
+		
+		if (result.hasErrors()) {
+			return "/departamento/cadastro";
+		}
+		
 		service.editar(departamento);
 		attr.addFlashAttribute("success", "Departamento editado com sucesso.");
 		return "redirect:/departamentos/cadastrar";
